8bb080daf359d1f9d651d0f88b7739c7e90e5a41
sayghteight <sayghteight@gmail.com>
2017-11-25 07:09:58 +0100

    Implemented news

--- application/modules/home/models/Home_model.php
+++ application/modules/home/models/Home_model.php
@@ -35,76 +35,11 @@ public function LogData($page, $data)
 
 // Function GrabNews
 
-public function GrabNews()
+public function getNews()
 {
-	$page    = isset($_GET['page']) ? (int)$_GET['page'] : 1;
-	$perPage = $this->config->item('news_results');
-
-	$start = ($page > 1) ? ($page * $perPage) - $perPage : 0;
-
-
-  $total = $this->db->query("SELECT COUNT (*) FROM news");
-	$total->execute();
-
-	$total = $total->fetchColumn();
-
-	$pages = ceil($total / $perPage);
-
-  $data = $this->db->query("SELECT * FROM news ORDER BY id DESC LIMIT ' . $start .', ' .$perpage");
-	$data->execute();
-
-	while($result = $data->fetchAll(PDO::FETCH_ASSOC))
-	{
-		foreach($result as $row)
-		{
-			echo '<div class="content-box column small-12">
-					<div class="content-box-header column small-12">
-						<div class="title-text">
-							' . $row['news_title'] . '
-						</div>
-
-						<div class="title-date">
-							' . $row['post_date'] . '
-						</div>
-					</div>
-
-					<div class="content-box-content column small-12">
-						<div class="content-text">
-							' . $row['news_content'] . '
-						</div>
-					</div>
-				</div>';
-		}
-	}
-
-	echo '<div class="navigation column small-12"><ul class="nav-box">';
-
-		echo '<li><a href="'.base_url().'?page=1"><<</a></li>';
-
-
-		$min = max($page - 2, 1);
-		$max = min($page + 2, $pages);
-
-		for($x = $min; $x <= $max; $x++)
-		{
-			if(@$page == $x)
-			{
-				echo '<li><a href="?page=' . $x . '" class="current-nav">' . $x . '</a></li>';
-			}
-			elseif(@!isset($page))
-			{
-				echo '<li><a href="?page=' . $x . '" class="current-nav">' . $x . '</a></li>';
-			}
-			else
-			{
-				echo '<li><a href="?page=' . $x . '">' . $x . '</a></li>';
-			}
-		}
-
-		echo '<li><a href="?page=' . $pages . '">>></a></li>';
-
-	echo '</ul></div>';
+return $this->db->query("SELECT * FROM news);
 }
+
 
 
 };
--- application/modules/home/views/home.php
+++ application/modules/home/views/home.php
@@ -1,9 +1,21 @@
 
 <div class="row">
-	<div class="content column small-12 medium-7 large-8">
-		<?php $this->home_model->GrabNews();   ?>
-	</div>
-
+  <div class="content-box column small-12">
+    <?php foreach($this->home_model>getNews()->result() as $list) { ?>
+      <div class="content-box-header column small-12">
+          <div class="title-text">
+              <?= $list->news_title ?> by <?= $list->news_author ?>
+          </div>
+          <div class="title-date">
+              <?= $list->post_date ?>
+          </div>
+      </div>
+      <div class="content-box-content column small-12">
+          <div class="content-text">
+              <?= $list->news_content ?>
+          </div>
+      </div>
+  </div>
 	<div class="content column small-12 medium-5 large-4">
 		<div class="content-box-sidebar column small-12">
 			<iframe src="https://discordapp.com/widget?id=355021208246288384&theme=dark" width="100%" height="500" allowtransparency="true" frameborder="0"></iframe>
