977f3a5d229c5371484e07333013c2ab527b6cf3
sayghteight <sayghteight@gmail.com>
2017-11-25 21:49:12 +0100

    Fixed string home

--- application/modules/home/controllers/Home.php
+++ application/modules/home/controllers/Home.php
@@ -5,7 +5,7 @@ class Home extends MX_Controller {
 
 	public function index()
 	{
-		$this->load->model('user/user_model.php')
+		$this->load->model('user/user_model.php');
 		$this->load->model('home_model');
     $this->load->view('header');
 		$this->load->view('home');
