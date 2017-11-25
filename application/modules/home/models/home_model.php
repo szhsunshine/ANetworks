88a04a7c67323eaadde7621bcef35afbb0d015b7
sayghteight <sayghteight@gmail.com>
2017-11-25 06:08:55 +0100

    [FIX/Home] Models

---
+++ application/modules/home/models/home_model.php
@@ -0,0 +1,34 @@
+<?php
+
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Home_model extends CI_Model {
+
+
+	 public function __construct()
+	 {
+		parent::__construct();
+	 }
+
+
+// Functions LOGS
+
+
+public function LogData($page, $data)
+{
+
+	$username   = $_SESSION['username'];
+	$user_agent = $_SERVER['HTTP_USER_AGENT'];
+	$ip_address = $_SERVER['REMOTE_ADDR'];
+
+
+  $this->db->query("INSERT INTO logs (username, page, data, user_agent, ip, time) VALUES (:username, :page, :data, :user_agent, :ip, :time)");
+  $this->execute(array(
+		':username'   => $username,
+		':page'       => $page,
+		':data'       => $data,
+		':user_agent' => $user_agent,
+		':ip'         => $ip_address,
+		':time'       => time()
+	));
+}
