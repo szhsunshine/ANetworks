e4c206014db8ee37dda23811c5714aea31e480ed
sayghteight <sayghteight@gmail.com>
2017-11-26 20:49:16 +0100

    Delete LogData functión (Home model)

--- application/modules/home/models/Home_model.php
+++ application/modules/home/models/Home_model.php
@@ -8,25 +8,6 @@ class Home_model extends CI_Model {
 		parent::__construct();
 	 }
 
-public function LogData($page, $data)
-{
-
-	$username   = $_SESSION['username'];
-	$user_agent = $_SERVER['HTTP_USER_AGENT'];
-	$ip_address = $_SERVER['REMOTE_ADDR'];
-
-
-  $this->db->query("INSERT INTO logs (username, page, data, user_agent, ip, time) VALUES (:username, :page, :data, :user_agent, :ip, :time)");
-  $this->execute(array(
-		':username'   => $username,
-		':page'       => $page,
-		':data'       => $data,
-		':user_agent' => $user_agent,
-		':ip'         => $ip_address,
-		':time'       => time()
-	));
-}
-
 public function getNews()
 {
 return $this->db->query("SELECT * FROM news");
