3b0cba6042c6b16fa8b5da99b5a709824f711556
krizttian <develop.km14@gmail.com>
2017-11-30 08:58:49 +0200

    Minimal fix

--- application/modules/home/controllers/Home.php
+++ application/modules/home/controllers/Home.php
@@ -1,13 +1,13 @@
-<?php
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Home extends MX_Controller {
-
 
 	public function index()
 	{
 		$this->load->model('user/user_model');
 		$this->load->model('home_model');
-    $this->load->view('header');
+		
+    	$this->load->view('header');
 		$this->load->view('home');
 		$this->load->view('footer');
 	}
