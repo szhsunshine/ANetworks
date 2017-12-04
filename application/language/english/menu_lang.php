251a6736e5787e2b0cc62f7cb3797b3dda35e4d6
DZywolf <luis.wolf.core@gmail.com>
2017-12-04 02:07:16 -0300

    Clean Language files and add index.html

--- application/language/english/menu_lang.php
+++ application/language/english/menu_lang.php
@@ -1,28 +1,47 @@
 <?php
+/**
+ * ANetworks
+ *
+ * An open source application based on CodeIgniter for PHP
+ *
+ * This content is released under the MIT License (MIT)
+ *
+ * Copyright (c) 2014 - 2017, British Columbia Institute of Technology
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+defined('BASEPATH') OR exit('No direct script access allowed');
 
-
-$lang['menu_news']		= 'News';
-$lang['menu_home']		= 'Home';
+$lang['menu_news'] = 'News';
+$lang['menu_home'] = 'Home';
 $lang['menu_database'] = 'Database';
-$lang['menu_support']	= 'Support';
-$lang['menu_login']		= 'Log In';
-$lang['menu_register']		= 'Register';
-$lang['menu_logout']		= 'Logout';
-$lang['my_account']		= 'My Account';
+$lang['menu_support'] = 'Support';
+$lang['menu_login'] = 'Log In';
+$lang['menu_register'] = 'Register';
+$lang['menu_logout'] = 'Logout';
+$lang['my_account'] = 'My Account';
 
-
-
-
-
-
-
-// Expansions lang
-
-
-$lang['exp_classic']		= 'Classic';
-$lang['exp_tbc']		= 'The Burning Crusade';
-$lang['exp_wtlk']		= 'Wrath of the Lich King';
-$lang['exp_cata']		= 'Cataclysm';
-$lang['exp_mop']		= 'Mist of Pandaria';
-$lang['exp_wod']		= 'Warlords of Draenor';
-$lang['exp_legion']		= 'Legion';
+/* Expansions lang */
+$lang['exp_classic'] = 'Classic';
+$lang['exp_tbc'] = 'The Burning Crusade';
+$lang['exp_wtlk'] = 'Wrath of the Lich King';
+$lang['exp_cata'] = 'Cataclysm';
+$lang['exp_mop'] = 'Mist of Pandaria';
+$lang['exp_wod'] = 'Warlords of Draenor';
+$lang['exp_legion'] = 'Legion';
