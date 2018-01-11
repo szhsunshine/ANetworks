bce2f96a46a92479799a816a8277d178056281c4
sayghteight <sayghteight@gmail.com>
2018-01-11 19:33:58 +0100

    Fixed lang error English

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -117,7 +117,7 @@ $autoload['config'] = array('');
 | "codeigniter_lang.php" would be referenced as array('codeigniter');
 |
 */
-$autoload['language'] = array('l_lang_general', 'l_lang_errors');
+$autoload['language'] = array('l_general', 'l_errors');
 
 /*
 | -------------------------------------------------------------------
